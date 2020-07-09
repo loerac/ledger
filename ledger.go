c80563ef4d1d8a40271efe2f13f7482f33ddb231
Christian Loera <chatoloera18@gmail.com>
2020-07-09 11:49:36 -0500

    Parse the ledger through the account number

--- ledger.go
+++ ledger.go
@@ -10,23 +10,27 @@ import (
 )
 
 type EntryItem struct {
-    Date string
-    Store string
-    Address string
-    Detail string
-    Exchange string
-    Cost float64
-    Balance float64
+    Date        string
+    Store       string
+    Address     string
+    Detail      string
+    Exchange    string
+    Cost        float64
+    Balance     float64
 }
 
 type Ledger struct {
-    NumEntries uint64
-    Filepath string
-    Entry []EntryItem
+    Filepath    string
+    AccountNum  map[string][]EntryItem
 }
 
 const (
-    LGR_DATE = iota
+    METADATA_LEN int = 7
+)
+
+const (
+    LGR_ACCOUNT_NUM = iota
+    LGR_DATE
     LGR_LOC
     LGR_DETAIL
     LGR_EXCHANGE
@@ -43,9 +47,8 @@ const (
  **/
 func NewLedger(lgrfp string) Ledger {
     lgr := Ledger{}
-    lgr.NumEntries = 0
     lgr.Filepath = lgrfp
-    fmt.Println("Filepath:", lgr.Filepath)
+    lgr.AccountNum = make(map[string][]EntryItem)
 
     return lgr
 }
@@ -53,73 +56,76 @@ func NewLedger(lgrfp string) Ledger {
 /**
  * @brief:  Parse the meta data of each line in the ledger, as seen below.
  *          Each data will be added to the ledger.
- *          <YYYYMMDDTHHMMSS>:<STORE>@<ADDRESS>:<DETAILS>:<EXCHANGE-TYPE>:<COST>:<BALANCE>
+ *          <ACCOUNT-NUM>:<YYYYMMDDTHHMMSS>:<STORE>@<ADDRESS>:<DETAILS>:<EXCHANGE-TYPE>:<COST>:<BALANCE>
  *
  * @arg:    data - meta data
  **/
 func (lgr *Ledger) ParseLedgerLine(data string) {
     split := strings.Split(data, ":")
-    entry := EntryItem{}
-
-    lgr.NumEntries += 1
-    entry.Date = split[LGR_DATE]
-    loc := strings.Split(split[LGR_LOC], "@")
-    entry.Store = loc[0]
-    entry.Address = ""
-    if 2 == len(loc) {
-        entry.Address = loc[1]
+    if (METADATA_LEN != len(split)) {
+        fmt.Println("Malformed entry:", data)
+        return
     }
-    entry.Detail = split[LGR_DETAIL]
-    entry.Exchange = split[LGR_EXCHANGE]
-    entry.Cost = StrToFloat(split[LGR_COST])
-    entry.Balance = StrToFloat(split[LGR_BALANCE])
 
-    lgr.Entry = append(lgr.Entry, entry)
+    date := split[LGR_DATE]
+    loc := strings.Split(split[LGR_LOC], "@")
+    store := loc[0]
+    address := ""
+    if 2 == len(loc) {
+        address = loc[1]
+    }
+    detail := split[LGR_DETAIL]
+    exchange := split[LGR_EXCHANGE]
+    cost := StrToFloat(split[LGR_COST])
+    balance := StrToFloat(split[LGR_BALANCE])
+
+    lgr.AccountNum[split[LGR_ACCOUNT_NUM]] =
+        append(
+            lgr.AccountNum[split[LGR_ACCOUNT_NUM]],
+            EntryItem{date, store, address, detail, exchange, cost, balance},
+        )
 }
 
 /**
  * @brief:  Create a new entry for the ledger. Will append entry to file.
  *
+ * @arg:    acctNum - Account Number of the entry
  * @arg:    store - Name of the store
  * @arg:    addr - Location of the store. Optional, can be left blank
  * @arg:    detail - Reasoning or brief detail
  * @arg:    exchange - Either true ("Income") or false ("Expense")
  * @arg:    cost - Gain or expense
  **/
-func (lgr *Ledger) AddEntry(store, addr, detail string, isIncome bool, cost float64) {
-    entry := EntryItem{}
+func (lgr *Ledger) AddEntry(acctNum, store, addr, detail string, isIncome bool, cost float64) {
     currTime := time.Now()
     date := fmt.Sprintf("%d%02d%02dT%02d%d%d",
             currTime.Year(), currTime.Month(), currTime.Day(),
             currTime.Hour(), currTime.Minute(), currTime.Second())
-    balance := lgr.Entry[lgr.NumEntries - 1].Balance + cost
+    balance := lgr.AccountNum[acctNum][len(lgr.AccountNum) - 1].Balance + cost
     exchange := "Expense"
     if isIncome {
         exchange = "Income"
     }
 
-    lgr.NumEntries += 1
-    entry.Date = date
-    entry.Store = store
-    entry.Address = addr
-    entry.Detail = detail
-    entry.Exchange = exchange
-    entry.Cost = cost
-    entry.Balance = balance
-    lgr.Entry = append(lgr.Entry, entry)
+    lgr.AccountNum[acctNum] =
+        append(
+            lgr.AccountNum[acctNum],
+            EntryItem{date, store, addr, detail, exchange, cost, balance},
+        )
 
     exchange += ":"
     if isIncome {
         exchange += "+"
     }
 
-    newEntry := fmt.Sprintf("%s:%s@%s:%s:%s%0.2f:%0.2f", date, store, addr, detail, exchange, cost, balance)
+    newEntry := fmt.Sprintf("%s:%s:%s@%s:%s:%s%0.2f:%0.2f", acctNum, date, store, addr, detail, exchange, cost, balance)
     if "" == addr {
-        newEntry = fmt.Sprintf("%s:%s:%s:%s%0.2f:%0.2f", date, store, detail, exchange, cost, balance)
+        newEntry = fmt.Sprintf("%s:%s:%s:%s:%s%0.2f:%0.2f", acctNum, date, store, detail, exchange, cost, balance)
     }
 
-    fmt.Println("Added new ledger entry:", newEntry)
-    lgr.PrintLedgerItem(lgr.NumEntries - 1)
+    fmt.Println("Added new ledger entry:")
+    lgr.PrintLedgerItem(lgr.AccountNum[acctNum][len(lgr.AccountNum) - 1])
+    fmt.Println()
     lgr.AddToLedger(newEntry)
 }
 
@@ -127,7 +133,7 @@ func (lgr *Ledger) AddEntry(store, addr, detail string, isIncome bool, cost floa
  * @brief:  Add the new entry to the ledger file
  *
  * @arg:    entry - Entry in the meta data format
- *          <YYYYMMDDTHHMMSS>:<STORE>@<ADDRESS>:<DETAILS>:<EXCHANGE-TYPE>:<COST>:<BALANCE>
+ *          <ACCOUNT-NUM>:<YYYYMMDDTHHMMSS>:<STORE>@<ADDRESS>:<DETAILS>:<EXCHANGE-TYPE>:<COST>:<BALANCE>
  **/
 func (lgr Ledger) AddToLedger(entry string) {
     f, err := os.OpenFile(lgr.Filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -165,26 +171,35 @@ func (lgr *Ledger) ReadLedger() {
  * @brief:  Print individual item for ledger. To print the complete ledger,
  *          run `PrintLedger()`
  *
- * @arg:    item - Item to print in the ledger
+ * @arg:    entry - Entry in the meta data format
  **/
-func (lgr Ledger) PrintLedgerItem(item uint64) {
-    if item > lgr.NumEntries {
-        return
-    }
-
-    entry := lgr.Entry
-    fmt.Println(entry[item].Date, entry[item].Store)
+func (lgr Ledger) PrintLedgerItem(entry EntryItem) {
+    fmt.Println(entry.Date, entry.Store)
 
     entitySign := ""
-    if entry[item].Exchange == "Income" {
+    if entry.Exchange == "Income" {
         entitySign = "+"
     }
-    fmt.Printf("\t\t%s: %s %s%f\n", entry[item].Exchange, entry[item].Detail, entitySign, entry[item].Cost)
-    fmt.Printf("\t\tBalance: %f\n", entry[item].Balance)
+    fmt.Printf("\t\t%s: %s %s%f\n", entry.Exchange, entry.Detail, entitySign, entry.Cost)
+    fmt.Printf("\t\tBalance: %f\n", entry.Balance)
 
-    if entry[item].Address != "" {
-        fmt.Println("\t\tLocation:", entry[item].Address)
+    if entry.Address != "" {
+        fmt.Println("\t\tLocation:", entry.Address)
     }
+}
+
+/**
+ * @brief:  Print the entire ledger for an account
+ *
+ * @arg:    acctNum - Account Number of the entry
+ **/
+func (lgr Ledger) PrintLedgerAccount(acctNum string) {
+    fmt.Println("Account Number:", acctNum)
+    fmt.Println("============================")
+    for _, v := range lgr.AccountNum[acctNum] {
+        lgr.PrintLedgerItem(v)
+    }
+    fmt.Println()
 }
 
 /**
@@ -192,8 +207,12 @@ func (lgr Ledger) PrintLedgerItem(item uint64) {
  *          run `PrintLedgerItem()`
  **/
 func (lgr Ledger) PrintLedger() {
-    var i uint64 = 0
-    for ; i < lgr.NumEntries; i++ {
-        lgr.PrintLedgerItem(i)
+    for key, val := range lgr.AccountNum {
+        fmt.Println("Account Number:", key)
+        fmt.Println("============================")
+        for _, v := range val {
+            lgr.PrintLedgerItem(v)
+        }
+        fmt.Println()
     }
 }
