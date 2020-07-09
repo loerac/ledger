72b92319454e29014d4052c60409a757a2cf4110
Christian Loera <chatoloera18@gmail.com>
2020-07-09 11:48:43 -0500

    Using account numbers
    For each entry, there is an account number with it so it can associate which account is being used. For now, not sure about the future, this is in a single ledger notebook.

--- example/main.go
+++ example/main.go
@@ -17,6 +17,10 @@ func main() {
     ledger.PrintLedger()
 
     /* Add to new items to the ledger, one expense and one income type */
-    ledger.AddEntry("Store store", "S 456 St., Small Town, Big State", "Shopping", false, -19)
-    ledger.AddEntry("Farm Big Lot", "", "Farm equipment", true, 19)
+    ledger.AddEntry("0x9e9b99ffd770cba8", "Store store", "S 456 St., Small Town, Big State", "Shopping", false, -19)
+    ledger.AddEntry("0x4ee87e41a13b2634", "Farm Big Lot", "", "Farm equipment", true, 19)
+
+    /* Print the information on an account */
+    ledger.PrintLedgerAccount("0x9e9b99ffd770cba8")
+    ledger.PrintLedgerAccount("0x4ee87e41a13b2634")
 }
