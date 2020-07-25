e577797b7efc6d7f954e8b3bad4e9b2791023469
loerac <chatoloera18@gmail.com>
2020-07-25 06:49:52 -0500

    Example of how the argument flag works
    This is only temp, will create function that'll do this and add it to the ledger package

--- example/main.go
+++ example/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+    "bufio"
+    "flag"
     "fmt"
+    "os"
+    "strings"
 
     lgr "github.com/loerac/ledger"
 )
@@ -9,31 +13,94 @@ import (
 const (
     LEDGER1 string = "notebook1.lgr"
     LEDGER2 string = "notebook2.lgr"
+    NOTEBOOK_DESC string = "The ledger notebook"
+    PRINTPRETTY_DESC string = "Print the ledger notebook pretty"
+    PRINTTABLE_DESC string = "Print the ledger notebook to a markdown table\nNote: You can pass an output markdown file name"
+    ADDENTRY_DESC string = "Add a new entry to the ledger notebook\nNote: Ledger notebook is required"
+    NEWACCOUNT_DESC string = "Create a new account"
 )
 
+var (
+    notebook    string
+    printPretty bool
+    printTable  string
+    addEntry    bool
+    createAccount  bool
+    transaction string
+    location    string
+    detail      string
+    cost        float64
+)
+
+func readBuf(prompt string, required bool) string {
+        text := ""
+        for {
+            reader := bufio.NewReader(os.Stdin)
+            fmt.Print(prompt + " > ")
+            text , _ = reader.ReadString('\n')
+            text = strings.Replace(text, "\n", "", -1)
+
+            if required && text == "" {
+                fmt.Println("Error: input required, none given")
+            } else {
+                break
+            }
+        }
+
+        return text
+}
+
 func main() {
+    flag.StringVar(&notebook, "ledger", "", NOTEBOOK_DESC)
+    flag.StringVar(&notebook, "l", "", NOTEBOOK_DESC + " (Short-hand)")
+    flag.BoolVar(&printPretty, "print-pretty", false, PRINTPRETTY_DESC)
+    flag.BoolVar(&printPretty, "pp", false, PRINTPRETTY_DESC + " (Short-hand)")
+    flag.StringVar(&printTable, "print-table", "", PRINTTABLE_DESC)
+    flag.StringVar(&printTable, "pt", "", PRINTTABLE_DESC + " (Short-hand)")
+    flag.BoolVar(&addEntry, "entry", false, ADDENTRY_DESC)
+    flag.BoolVar(&addEntry, "e", false, ADDENTRY_DESC + " (Short-hand)")
+    flag.BoolVar(&createAccount, "account", false, NEWACCOUNT_DESC)
+    flag.BoolVar(&createAccount, "a", false, NEWACCOUNT_DESC + " (Short-hand)")
+    flag.Parse()
+
+    if notebook == "" && !createAccount {
+        fmt.Println("Error: Missing ledger notebook")
+        flag.PrintDefaults()
+        os.Exit(1)
+    }
+
     /* Get a new init ledger struct */
     ledger := lgr.NewLedger()
 
-    /* Read the ledger notebook from above */
-    ledger.ReadLedger(LEDGER1, LEDGER2)
+    /* Create a new account */
+    if createAccount {
+        fullname := readBuf("Enter the full name of the new account", true)
+        balance := lgr.StrToFloat(readBuf("Enter initial balance for " + fullname + " account", true))
+        notebook = fullname + ".lgr"
+        notebook = strings.ToLower(strings.Replace(notebook, " ", "-", -1))
+        ledger.CreateAccountHash(fullname, notebook, balance)
+    }
 
-    /* Print the ledger out pretty */
-    ledger.PrintLedger()
+    /* Read the ledger notebook from above */
+    ledger.ReadLedger(notebook)
+    acctNum := ledger.GetAcctNum(notebook)
 
     /* Add to new items to the ledger, one expense and one income type */
-    ledger.AddEntry("5524c5d66aeee973", "Store store", "S 456 St., Small Town, Big State", "Shopping", -19)
-    ledger.AddEntry("936e1204e7b8c686", "Farm Big Lot", "", "Farm equipment", 19)
+    if addEntry {
+        transaction = readBuf("Enter area of transaction", true)
+        location = readBuf("Enter area of location of transaction", false)
+        detail = readBuf("Enter details of transaction", true)
+        cost = lgr.StrToFloat(readBuf("Enter cost of transaction", true))
+        ledger.AddEntry(acctNum, transaction, location, detail, cost)
+    }
 
-    /* Print the information on an account */
-    ledger.PrintLedgerAccount("5524c5d66aeee973")
+    /* Print the ledger out pretty */
+    if printPretty {
+        ledger.PrintLedger()
+    }
 
     /* Print the information into a markdown table */
-    ledger.PrintToTable("936e1204e7b8c686", "ledger-table")
-
-    /* Add a new account */
-    fullname := "Jimmy Johns"
-    acctNum := ledger.CreateAccountHash(fullname, "notebook3.lgr", 12.90)
-    fmt.Println("New account number for", fullname, "-", acctNum)
-    ledger.AddEntry(acctNum, "Subway", "Subway 456 Rd., Subs City, Subs State", "Subs", -6.12)
+    if printTable != "" {
+        ledger.PrintToTable(acctNum, printTable)
+    }
 }
