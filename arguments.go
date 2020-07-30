717c30fbb96c45d199d2087f69fbf377aa360a69
loerac <chatoloera@gmail.com>
2020-07-30 17:01:16 -0500

    Added comments and moved variables around

--- arguments.go
+++ arguments.go
@@ -16,37 +16,49 @@ const (
     NEWACCOUNT_DESC string = "Create a new account"
 )
 
-var (
-    notebook    string
-    printPretty bool
-    printTable  string
-    addEntry    bool
-    createAccount  bool
-    transaction string
-    location    string
-    detail      string
-    cost        float64
-)
-
+/***
+ * @brief:  Reads input from the user
+ *
+ * @arg:    prompt - String to ask the user
+ * @arg:    required - Input is required if true
+ *
+ * @return: The user response from the prompt
+ ***/
 func readBuf(prompt string, required bool) string {
-        text := ""
+        response := ""
         for {
             reader := bufio.NewReader(os.Stdin)
             fmt.Print(prompt + " > ")
-            text , _ = reader.ReadString('\n')
-            text = strings.Replace(text, "\n", "", -1)
+            response , _ = reader.ReadString('\n')
+            response = strings.Replace(response, "\n", "", -1)
 
-            if required && text == "" {
+            if required && response == "" {
                 fmt.Println("Error: input required, none given")
             } else {
                 break
             }
         }
 
-        return text
+        return response
 }
 
+/***
+ * @brief:  Checks to see if user has passed any flags.
+ *          -l: The ledger notebook
+ *          -pp: Print the ledger notebook out pretty
+ *          -pt: Save the ledger notebook as a markdown table
+ *          -add-entry: Adds an entry to the ledger notebook
+ *          -new-acct: Creates a new account and ledger notebook
+ ***/
 func (lgr *Ledger) ArgumentParser() {
+    var (
+        notebook    string
+        printPretty bool
+        printTable  string
+        addEntry    bool
+        createAccount  bool
+    )
+
     flag.StringVar(&notebook, "l", "", LEDGER_DESC)
     flag.BoolVar(&printPretty, "pp", false, PRINTPRETTY_DESC)
     flag.StringVar(&printTable, "pt", "", PRINTTABLE_DESC)
@@ -60,10 +72,6 @@ func (lgr *Ledger) ArgumentParser() {
         os.Exit(1)
     }
 
-    /* Read the ledger notebook from above */
-    lgr.ReadLedger(notebook)
-    acctNum := lgr.GetAcctNum(notebook)
-
     /* Create a new account */
     if createAccount {
         fullname := readBuf("Enter the full name of the new account", true)
@@ -73,12 +81,16 @@ func (lgr *Ledger) ArgumentParser() {
         lgr.CreateAccountHash(fullname, notebook, balance)
     }
 
+    /* Read the ledger notebook from above */
+    lgr.ReadLedger(notebook)
+    acctNum := lgr.GetAcctNum(notebook)
+
     /* Add to new items to the ledger, one expense and one income type */
     if addEntry {
-        transaction = readBuf("Enter area of transaction", true)
-        location = readBuf("Enter area of location of transaction", false)
-        detail = readBuf("Enter details of transaction", true)
-        cost = StrToFloat(readBuf("Enter cost of transaction", true))
+        transaction := readBuf("Enter area of transaction", true)
+        location := readBuf("Enter area of location of transaction", false)
+        detail := readBuf("Enter details of transaction", true)
+        cost := StrToFloat(readBuf("Enter cost of transaction", true))
         lgr.AddEntry(acctNum, transaction, location, detail, cost)
     }
 
